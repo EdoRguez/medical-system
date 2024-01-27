134a87dae71d59b5d95a82271551cfe1ac02f7e8
edorguez <edro998@gmail.com>
2024-01-27 16:22:20 -0400

    Downloaded mac grpc files and created config file

--- src/backend/patient-svc/cmd/main.go
+++ src/backend/patient-svc/cmd/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/EdoRguez/medical-system/patient-svc/pkg/config"
+	"github.com/EdoRguez/medical-system/patient-svc/pkg/db"
+	"github.com/EdoRguez/medical-system/patient-svc/pkg/pb"
+	"github.com/EdoRguez/medical-system/patient-svc/pkg/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBSource)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Patient Svc on", c.Port)
+
+	s := services.Server{
+		H: h,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterPatientServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
 }
